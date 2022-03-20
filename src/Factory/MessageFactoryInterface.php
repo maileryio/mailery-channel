d8f65ddff9669df193ef7e823caf32937fab099f
Igor Maliy <imaliy.filsh@gmail.com>
2022-03-21 00:26:04 +0200

    Development

--- src/Factory/MessageFactoryInterface.php
+++ src/Factory/MessageFactoryInterface.php
@@ -2,13 +2,6 @@
 
 namespace Mailery\Channel\Factory;
 
-use Mailery\Template\Entity\Template;
-
 interface MessageFactoryInterface
 {
-    /**
-     * @param Template $template
-     * @return self
-     */
-//    public function withTemplate(Template $template): self;
 }
