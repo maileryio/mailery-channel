2d6491f1a9f34cd18e307b892c72538f301c56d6
Igor Maliy <imaliy.filsh@gmail.com>
2022-03-22 14:49:03 +0200

    Development

--- views/default/index.php
+++ views/default/index.php
@@ -3,19 +3,15 @@
 use Mailery\Activity\Log\Widget\ActivityLogLink;
 use Mailery\Icon\Icon;
 use Mailery\Channel\Entity\Channel;
-use Mailery\Widget\Dataview\Columns\ActionColumn;
-use Mailery\Widget\Dataview\Columns\DataColumn;
-use Mailery\Widget\Dataview\GridView;
-use Mailery\Widget\Dataview\GridView\LinkPager;
 use Mailery\Widget\Link\Link;
 use Mailery\Widget\Search\Widget\SearchWidget;
 use Yiisoft\Html\Html;
+use Yiisoft\Yii\DataView\GridView;
 
 /** @var Yiisoft\Yii\WebView $this */
 /** @var Mailery\Widget\Search\Form\SearchForm $searchForm */
 /** @var Yiisoft\Aliases\Aliases $aliases */
-/** @var Yiisoft\Router\UrlGeneratorInterface $urlGenerator */
-/** @var Yiisoft\Data\Reader\DataReaderInterface $dataReader*/
+/** @var Yiisoft\Router\UrlGeneratorInterface $url */
 /** @var Yiisoft\Data\Paginator\PaginatorInterface $paginator */
 /** @var Mailery\Channel\Model\ChannelTypeList $channelTypes */
 
@@ -46,7 +42,7 @@ $this->setTitle('All channels');
                             'b-dropdown-item',
                             $channelType->getCreateLabel(),
                             [
-                                'href' => $urlGenerator->generate($channelType->getCreateRouteName(), $channelType->getCreateRouteParams()),
+                                'href' => $url->generate($channelType->getCreateRouteName(), $channelType->getCreateRouteParams()),
                             ]
                         );
                     } ?>
@@ -59,7 +55,7 @@ $this->setTitle('All channels');
 <div class="row">
     <div class="col-12">
         <?= GridView::widget()
-            ->paginator($paginator)
+            ->layout("{items}\n<div class=\"mb-4\"></div>\n{summary}\n<div class=\"float-right\">{pager}</div>")
             ->options([
                 'class' => 'table-responsive',
             ])
@@ -70,79 +66,42 @@ $this->setTitle('All channels');
             ->emptyTextOptions([
                 'class' => 'text-center text-muted mt-4 mb-4',
             ])
+            ->paginator($paginator)
+            ->currentPage($paginator->getCurrentPage())
             ->columns([
-                (new DataColumn())
-                    ->header('Name')
-                    ->content(function (Channel $data, int $index) use ($urlGenerator) {
-                        return Html::a(
-                            $data->getName(),
-                            $urlGenerator->generate($data->getViewRouteName(), $data->getViewRouteParams())
-                        );
-                    }),
-                (new ActionColumn())
-                    ->contentOptions([
-                        'style' => 'width: 80px;',
-                    ])
-                    ->header('Edit')
-                    ->view('')
-                    ->update(function (Channel $data, int $index) use ($urlGenerator) {
+                [
+                    'label()' => ['Name'],
+                    'value()' => [fn (Channel $model) => Html::a($model->getName(), $url->generate($model->getViewRouteName(), $model->getViewRouteParams()))],
+                ],
+                [
+                    'label()' => ['Edit'],
+                    'value()' => [static function (Channel $model) use ($url) {
                         return Html::a(
                             Icon::widget()->name('pencil')->render(),
-                            $urlGenerator->generate($data->getEditRouteName(), $data->getEditRouteParams()),
+                            $url->generate($model->getEditRouteName(), $model->getEditRouteParams()),
                             [
                                 'class' => 'text-decoration-none mr-3',
                             ]
                         )
                         ->encode(false);
-                    })
-                    ->delete(''),
-                (new ActionColumn())
-                    ->contentOptions([
-                        'style' => 'width: 80px;',
-                    ])
-                    ->header('Delete')
-                    ->view('')
-                    ->update('')
-                    ->delete(function (Channel $data, int $index) use ($csrf, $urlGenerator) {
+                    }],
+                ],
+                [
+                    'label()' => ['Delete'],
+                    'value()' => [static function (Channel $model) use ($csrf, $url) {
                         return Link::widget()
                             ->csrf($csrf)
                             ->label(Icon::widget()->name('delete')->options(['class' => 'mr-1'])->render())
                             ->method('delete')
-                            ->href($urlGenerator->generate($data->getDeleteRouteName(), $data->getDeleteRouteParams()))
+                            ->href($url->generate($model->getDeleteRouteName(), $model->getDeleteRouteParams()))
                             ->confirm('Are you sure?')
                             ->options([
                                 'class' => 'text-decoration-none text-danger',
                             ])
                             ->encode(false);
-                    }),
+                    }],
+                ],
             ]);
         ?>
     </div>
-</div><?php
-if ($paginator->getTotalItems() > 0) {
-            ?><div class="mb-4"></div>
-    <div class="row">
-        <div class="col-6">
-            <?= GridView\OffsetSummary::widget()
-                ->paginator($paginator); ?>
-        </div>
-        <div class="col-6">
-            <?= LinkPager::widget()
-                ->paginator($paginator)
-                ->options([
-                    'class' => 'float-right',
-                ])
-                ->prevPageLabel('Previous')
-                ->nextPageLabel('Next')
-                ->urlGenerator(function (int $page) use ($urlGenerator) {
-                    $url = $urlGenerator->generate('/channel/default/index');
-                    if ($page > 1) {
-                        $url = $url . '?page=' . $page;
-                    }
-
-                    return $url;
-                }); ?>
-        </div>
-    </div><?php
-        }
-?>
+</div>
